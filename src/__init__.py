a86a66005278ba016f6348bea15378ec444da6f2
SaiRamC123-com <chutkeychethan@gmail.com>
2026-06-05 21:31:59 +0530

    Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,1 +1,7 @@
+# src/__init__.py
+from .data_loader import DataLoader
+from .inventory_analysis import InventoryAnalysis
+from .visualizations import Visualizations
 
+__all__ = ["DataLoader", "InventoryAnalysis", "Visualizations"]
+__version__ = "1.0.0"
